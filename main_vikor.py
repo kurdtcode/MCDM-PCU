02957d36c37ec26afa3a6d2dc40feeaf406ecd9d
wiwi101201 <william.w.1012@gmail.com>
2023-05-18 10:47:48 +0700

    Button vikor promethee sudah konek
    button"e dah konek

--- main_vikor.py
+++ main_vikor.py
@@ -11,6 +11,7 @@ from tabulate import tabulate
 
 app = Flask(__name__)
 
+
 def csv_to_matrix(file):
     with open(file, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -19,7 +20,13 @@ def csv_to_matrix(file):
     matrix = np.array(data, dtype=float)
     return matrix
 
+
 @app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['csv_file']
@@ -27,20 +34,33 @@ def upload_file():
             # Simpan file ke tempat temporary, soale nek pake request.file bakal error (minta fileStorage)
             temp_filepath = os.path.join(tempfile.gettempdir(), file.filename)
             file.save(temp_filepath)
-            
+
+            if(request.form['type'] == "vikor"):
+                print("vikor")
+                vikor_results = to_vikor(temp_filepath)
+                # promethee_results = to_promethee(temp_filepath)
+
+                # Yang temporary tadi hapus
+                os.remove(temp_filepath)
+
+                # hasil di print ke html
+                return render_template('result.html', vikor_results=vikor_results)
+            elif(request.form["type"] == "promethee"):
+                print("prom")
+                promethee_results = to_promethee(temp_filepath)
+                # promethee_results = to_promethee(temp_filepath)
+
+                # Yang temporary tadi hapus
+                os.remove(temp_filepath)
+
+                # hasil di print ke html
+                return render_template('result.html', vikor_results=promethee_results)
+
             # dibuat model vikor
-            vikor_results = to_vikor(temp_filepath)
-            # promethee_results = to_promethee(temp_filepath)
-            
-            # Yang temporary tadi hapus
-            os.remove(temp_filepath)
-            
-            # hasil di print ke html
-            return render_template('result.html', vikor_results=vikor_results)
-        
-    
+
     # render tempat upload
     return render_template('upload.html')
+
 
 def to_vikor(file):
     matrix = csv_to_matrix(file)
@@ -58,17 +78,19 @@ def to_vikor(file):
         vikor_results[name] = function(matrix, weight, criteria)
     return vikor_results
 
-# def to_promethee(file):
-#     matrix = csv_to_matrix(file)
-#     criteria = np.array([1, -1, -1, -1])
-#     promethee_methods = {
-#         'PROMETHEE II': mcdm_methods.promethee.PROMETHEE_II()
-#     }
-#     promethee_results = {}
-#     for name, method in promethee_methods.items():
-#         rankings = method.compute(matrix, criteria)
-#         promethee_results[name] = rankings
-#     return promethee_results
+
+def to_promethee(file):
+    matrix = csv_to_matrix(file)
+    criteria = np.array([1, -1, -1, -1])
+    promethee_methods = {
+        'PROMETHEE II': mcdm_methods.promethee.PROMETHEE_II()
+    }
+    promethee_results = {}
+    for name, method in promethee_methods.items():
+        rankings = method.compute(matrix, criteria)
+        promethee_results[name] = rankings
+    return promethee_results
+
 
 if __name__ == '__main__':
     app.run()
